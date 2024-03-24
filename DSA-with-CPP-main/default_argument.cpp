b1ebeafc04d8088ed4fb3e1d47949aeec4ae19d4
vishalrathod19 <vr3204917@gmail.com>
2024-03-24 10:58:27 +0530

    full code write

--- DSA-with-CPP-main/default_argument.cpp
+++ DSA-with-CPP-main/default_argument.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 void print( int arr[],int n , int size = 0 )
 {
-    
+    for(int i = 0 ; i<n ; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
+int main ()
+{
+    int arr[5]={2 ,4,5,6,7};
+    int n = 5;
+    print(arr,n);
+    print(arr,n,2);
+    return 0 ;
 }
