51dbcfac4f59ce3fa6fa718c498b4492aeb20e15
vishalrathod19 <vr3204917@gmail.com>
2024-03-24 22:57:42 +0530

    commit2

--- DSA-with-CPP-main/tempCodeRunnerFile.cpp
+++ DSA-with-CPP-main/tempCodeRunnerFile.cpp
@@ -1,44 +1,21 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int binary_search(vector<vector<int>>&matrix,int row , int col ,int target)
+void print(int n)
 {
-    int start = 0 ;
-    int end = ((row*col)-1);
-    while(start<=end)
+    if(n==0 )
     {
-        int mid = start+(end-start)/2;
-        int element = matrix[mid/col][mid%col];
-        if(element ==target)
-        {
-            return 1 ;
-        }
-        else if (element<target)
-        {
-            start = mid+1;
-
-        }
-        else{
-            end  = mid -1;
-        }
+        return; 
     }
-    return -1;
+    print(n-1);//head recursion
+    cout<<n<<endl;
 }
-int main ()
-{   int row = 3;  
-    int col = 3;
-    vector<vector<int>> matrix = {{1, 2, 3},
-                                   {4, 5, 6},
-                                   {7, 8, 9}}; 
-  int target = 10;
-  int result = binary_search(matrix,row,col,target);
-   if(result!=-1)
-    {
-        cout<<"the element found at index :- "<<result<<endl;
-    }
-    else {
-        cout<<"The element is not found"<<endl;
-    }
-    return 0 ;
 
+int main()
+{
+    int n;
+    cin>>n;
+    
+    print(n);
+    return 0;
 }
