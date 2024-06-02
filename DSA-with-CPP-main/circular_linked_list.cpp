03dc9583b8c7f210ad515083fac0b4f83dec057d
vishalrathod19 <vr3204917@gmail.com>
2024-06-02 23:24:58 +0530

    modification

--- DSA-with-CPP-main/circular_linked_list.cpp
+++ DSA-with-CPP-main/circular_linked_list.cpp
@@ -2,7 +2,7 @@
 using namespace std ;
 
 class Node {
-    public:
+public:
     int data;
     Node * next;
     //constructor 
@@ -24,6 +24,48 @@ class Node {
     }
 };
 
+void insertNode(Node * &tail,int element,int d)
+{
+   //assuming that the element is present in the last
+   //empty list 
+   if(tail == NULL)
+   {
+      Node* newNode = new Node(d);
+      tail = newNode;
+      newNode->next = newNode;
+   }
+   else {
+         //non empty list 
+         //assuming that the element is present in the list 
+         
+         Node* curr = tail ;
+         while (curr->data!= element ){
+              curr = curr-> next ;
+         }
+         //element found -> curr is representing element wala node
+         Node * temp = new Node(d);
+         temp->next  = curr-> next ;
+         curr->next = temp; 
+   }
+}
+void print(Node* tail)
+{
+     Node* temp = tail ;
+    do{
+        cout<<tail->data <<" ";
+        tail = tail -> next ;
+    }
+    while(tail != temp );
+    cout<<endl;
+}
 int main(){
      
+     Node * tail= NULL;
+     insertNode(tail,5,3);
+     print (tail);
+    insertNode(tail,3,5);
+     print (tail);
+      insertNode(tail,5,7);
+     print (tail);
+     return 0 ;
 }
